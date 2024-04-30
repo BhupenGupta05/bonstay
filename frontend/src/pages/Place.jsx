0a51604a70ec1fcc08543d9aa2b79496e08f5795
Bhupen Gupta <bhupengupta05@gmail.com>
2024-04-30 20:33:51 +0530

    prod build

--- frontend/src/pages/Place.jsx
+++ frontend/src/pages/Place.jsx
@@ -44,8 +44,8 @@ const Place = () => {
 
     if (loading) {
         // Display loading indicator while data is being loaded
-        return (<
-            div className="text-center mt-4">
+        return (
+        <div className="text-center mt-4">
                 <svg className="animate-spin h-8 w-8 text-primary mx-auto" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M12 2v2M12 20v2M4.93 4.93l1.41 1.41M16.24 16.24l1.41-1.41M4.93 19.07l1.41-1.41M16.24 7.76l1.41 1.41M2 12h2M20 12h2M4.93 12h2.14M16.24 12h2.14" />
                 </svg>
